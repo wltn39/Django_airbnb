1686c85efa05c27f89910838c31d0314a7511115
IngCoding <wltn39@gmail.com>
2022-07-06 17:02:52 +0900

    #5.3 Conversations and Messages Model

---
+++ lists/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from . import models 
+
+@admin.register(models.List)
+class ListAdmin(admin.ModelAdmin):
+
+    """ List Admin Definition """
+    pass
+
---
+++ reservations/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from . import models
+
+@admin.register(models.Reservation)
+class ReservationAdmin(admin.ModelAdmin):
+
+    """ Reservation Admin Definition """
+
+    pass
