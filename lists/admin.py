726bdba7f27697499d4aa3113951da50d6677e84
IngCoding <wltn39@gmail.com>
2022-07-12 11:10:43 +0900

    #8.2 Conversations, List, Reservations Admin

--- lists/admin.py
+++ lists/admin.py
@@ -5,5 +5,9 @@ from . import models
 class ListAdmin(admin.ModelAdmin):
 
     """ List Admin Definition """
-    pass
+    list_display = ("name", "user", "count_rooms")
+
+    search_fields = ("name",)
+
+    filter_horizontal = ("rooms",)
 
