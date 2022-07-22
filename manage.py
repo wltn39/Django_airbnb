d33e6379512d67e8cb31a49ca6a150e56c2054cc
IngCoding <wltn39@gmail.com>
2022-07-22 09:12:52 +0900

    #16.1 Dotenv

--- manage.py
+++ manage.py
@@ -3,6 +3,9 @@
 import os
 import sys
 
+import dotenv
+
+
 
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
@@ -18,4 +21,5 @@ def main():
 
 
 if __name__ == '__main__':
+    dotenv.read_dotenv()
     main()
